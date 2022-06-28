dd497050575bc99838a4b5ddb04ef7b25bc704b2
Paul Kristopher Espina <paulespina@xolveit.com>
2022-06-28 22:39:45 +0800

    user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'username'      => 'admin_espina',
+            'first_name'    => 'Paul',
+            'last_name'     => 'Espina',
+            'password'      => bcrypt('espina'),
+            'role'          => 'admin'
+        ]);
+
+        User::create([
+            'username'      => 'admin_punzalan',
+            'first_name'    => 'Neil',
+            'last_name'     => 'Punzalan',
+            'password'      => bcrypt('punzalan'),
+            'role'          => 'admin'
+        ]);
+
+        User::create([
+            'username'      => 'admin_conales',
+            'first_name'    => 'James',
+            'last_name'     => 'Conales',
+            'password'      => bcrypt('conales'),
+            'role'          => 'admin'
+        ]);
+
+        User::create([
+            'username'      => 'admin_luna',
+            'first_name'    => 'Korj',
+            'last_name'     => 'Luna',
+            'password'      => bcrypt('luna'),
+            'role'          => 'admin'
+        ]);
+
+        User::create([
+            'username'      => 'admin_amis',
+            'first_name'    => 'Medrick',
+            'last_name'     => 'Amis',
+            'password'      => bcrypt('amis'),
+            'role'          => 'admin'
+        ]);
+
+        User::create([
+            'username'      => 'user_test',
+            'first_name'    => 'Elisa',
+            'last_name'     => 'Malasaga',
+            'password'      => bcrypt('malasaga'),
+        ]);
+    }
+}

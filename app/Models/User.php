73ff714aa4a219277dda1e041392553e82dad288
Paul Kristopher Espina <paulespina@xolveit.com>
2022-06-28 22:48:18 +0800

    modify user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'username',
         'password',
     ];
 
@@ -31,14 +33,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }

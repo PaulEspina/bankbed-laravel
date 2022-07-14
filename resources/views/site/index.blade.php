2893e8c61366808bcef4339c959e8a8110165ca6
Paul Kristopher Espina <paulespina@xolveit.com>
2022-07-14 12:57:41 +0800

    site index UI

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -3,17 +3,30 @@
 @section('title', 'Welcome')
  
 @section('content')
-    <h1>Welcome</h1>
-    <button>
-        <a href="{{ route('site.profile') }}">Profile</a>
-    </button>
-    <button>
-        <a href="{{ route('site.withdraw') }}">Withdraw</a>
-    </button>
-    <button>
-        <a href="{{ route('site.deposit') }}">Deposit</a>
-    </button>
-    <button>
-        <a href="{{ route('site.transfer') }}">Transfer</a>
-    </button>
+<div class="cards-container">
+    <div class="card card-one">
+        <header>
+            <div class="avatar">
+                <img src="{{ asset('storage/Logo_Black_Square.jpg') }}" alt="Logo" />
+            </div>
+        </header>
+        <h3>{{ Auth::user()->first_name . " " . (Auth::user()->middle_name ? Auth::user()->middle_name . " " : "") . Auth::user()->last_name }}</h3>
+        <div class="desc">
+            <p class="Name"><b>Welcome {{ Auth::user()->username }}!</b></p>
+            <a href="{{ route('site.profile') }}" class="btn btn-primary">Profile</a>
+            <a href="{{ route('site.withdraw') }}" class="btn btn-primary">Withdraw</a>
+            <a href="{{ route('site.deposit') }}" class="btn btn-primary">Deposit</a>
+            <a href="{{ route('site.transfer') }}" class="btn btn-primary">Transfer</a>
+            <a href="/logout" class="btn btn-primary">Logout</a>
+        </div>
+    </div>
+</div>
+
+@push('head')
+    <link rel="stylesheet" href="{{ asset('css/profile_styles.css')}}" />
+@endpush
+
+@push('scripts')
+@endpush
+
 @endsection

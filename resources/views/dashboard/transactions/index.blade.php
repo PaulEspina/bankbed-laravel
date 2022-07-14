dd9b93e7c5b52906bc09dc38f9db94896121aac7
Paul Kristopher Espina <paulespina@xolveit.com>
2022-07-14 11:35:37 +0800

    transaction view UI

--- resources/views/dashboard/transactions/index.blade.php
+++ resources/views/dashboard/transactions/index.blade.php
@@ -3,30 +3,24 @@
 @section('title', 'Transactions')
  
 @section('content')
-    <h1>Transactions</h1>
-    
-    <button>
-        <a href="{{ route('dashboard.index') }}">Back</a>
-    </button>
-    <div>
-        <table>
-            <thead>
-                <tr>
-                <th>ID</th>
+<div class="header_fixed">
+    <table>
+        <thead>
+            <tr>
+                <th>ID No.</th>
                 <th>User</th>
                 <th>Sender</th>
                 <th>Receiver</th>
                 <th>Amount</th>
                 <th>Running Balance</th>
                 <th>Type</th>
-                <th>Updated At</th>
                 <th>Created At</th>
-                <th width="1%">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($transactions as $key => $transaction)
-                <tr key={{ $key }}>
+                <th>Updated At</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($transactions as $key => $transaction)
+            <tr key ={{ $key }}>
                 <td>{{ $transaction->id }}</td>
                 <td>{{ $transaction->username }}</td>
                 <td>{{ $transaction->sender }}</td>
@@ -36,19 +30,24 @@
                 <td>{{ $transaction->type }}</td>
                 <td>{{ date_format($transaction->updated_at,'m/d/Y') }}</td>
                 <td>{{ date_format($transaction->created_at,'m/d/Y') }}</td>
-                <td>
-                    <button>
-                        <a href="{{ route('dashboard.transactions.show', $transaction->id) }}">Show</a>
-                    </button>
-                </td>
-                </tr>
-                @empty
-                <tr>
+            </tr>
+            @empty
+            <tr>
                 <td colspan="100%">---</td>
-                </tr>
-                @endforelse
-            </tbody>
-        </table>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
+    <div class="primary-button">
+        <a href="{{ route('dashboard.index') }}"><button class="btn-primary" type="button">Back</button></a>
     </div>
+</div>
+
+@push('head')
+    <link rel="stylesheet" href="{{ asset('css/table_style.css')}}" />
+@endpush
+
+@push('scripts')
+@endpush
 
 @endsection

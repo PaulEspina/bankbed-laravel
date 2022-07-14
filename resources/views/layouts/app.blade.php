44c22d53688a504fd061420ac17508b55ab3304c
Paul Kristopher Espina <paulespina@xolveit.com>
2022-07-14 14:36:20 +0800

    change app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Banking System - @yield('title')</title>
+        <title>{{ env('APP_NAME') }} - @yield('title')</title>
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" >
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>

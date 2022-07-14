bd23d8719b6d0cdfd931ed4080a8e7d365b68456
Paul Kristopher Espina <paulespina@xolveit.com>
2022-07-14 13:35:52 +0800

    cleanup

--- resources/views/site/deposit.blade.php
+++ resources/views/site/deposit.blade.php
@@ -6,29 +6,27 @@
 <div class="container-fluid bg">
     <div class="row">
         <div class="col-md-6 offset-md-3">
-            <div class="signup-form">
-                {{Form::open(['route' => 'site.deposit.submit', 'class' => 'form-container mt-5 border p-4 bg-light shadow'])}}
-                    <a href="{{ route('site.index') }}"><button type="button" class="btn-close" aria-label="Close"></button></a>
-                    <h3 class="my-3 text-secondary">Deposit</h3>
-                    @if(Session::has('message'))
-                        <p class="alert alert-success">{{Session::get('message')}}</p>
-                    @endif
-                    <div class="row-mx-5">
-                        <div class="form-floating">
-                            {{Form::select('account_number', $bankAccounts, null, ['class' => 'form-select'])}}
-                            <label for="account_number">Account</label>
-                        </div>
-
-                        <div class="form-floating my-3">
-                            {{Form::number('amount', null, ['class' => 'form-control', 'placeholder' => 200.00])}}
-                            <label for="amount">Amount</label>
-                        </div>
-                        <div class="d-grid gap-2">
-                            <button class="btn btn-primary" type="submit">DEPOSIT</button>
-                        </div>
+            {{Form::open(['route' => 'site.deposit.submit', 'class' => 'form-container mt-5 border p-4 bg-light shadow'])}}
+                <a href="{{ route('site.index') }}"><button type="button" class="btn-close" aria-label="Close"></button></a>
+                <h3 class="my-3 text-secondary">Deposit</h3>
+                @if(Session::has('message'))
+                    <p class="alert alert-success">{{Session::get('message')}}</p>
+                @endif
+                <div class="row-mx-5">
+                    <div class="form-floating">
+                        {{Form::select('account_number', $bankAccounts, null, ['class' => 'form-select'])}}
+                        <label for="account_number">Account</label>
                     </div>
-                {{Form::close()}}
-            </div>
+
+                    <div class="form-floating my-3">
+                        {{Form::number('amount', null, ['class' => 'form-control', 'placeholder' => 200.00])}}
+                        <label for="amount">Amount</label>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-primary" type="submit">DEPOSIT</button>
+                    </div>
+                </div>
+            {{Form::close()}}
         </div>
     </div>
 </div>
--- resources/views/site/transfer.blade.php
+++ resources/views/site/transfer.blade.php
@@ -6,32 +6,30 @@
 <div class="container-fluid bg">
     <div class="row">
         <div class="col-md-6 offset-md-3">
-            <div class="signup-form">
-                {{Form::open(['route' => 'site.transfer.submit', 'class' => 'form-container mt-5 border p-4 bg-light shadow'])}}
-                    <a href="{{ route('site.index') }}"><button type="button" class="btn-close" aria-label="Close"></button></a>
-                    <h3 class="my-3 text-secondary">Withdraw</h3>
-                    @if(Session::has('message'))
-                        <p class="alert alert-success">{{Session::get('message')}}</p>
-                    @endif
-                    <div class="row-mx-5">
-                        <div class="form-floating">
-                            {{Form::select('account_number', $bankAccounts, null, ['class' => 'form-select'])}}
-                            <label for="account_number">Account</label>
-                        </div>
-                        <div class="form-floating my-3">
-                            {{Form::text('receiver', null, ['class' => 'form-control', 'placeholder' => 'Ex. 2022-321123-0'])}}
-                            <label for="receiver">Transfer To</label>
-                        </div>
-                        <div class="form-floating my-3">
-                            {{Form::number('amount', null, ['class' => 'form-control', 'placeholder' => 200.00])}}
-                            <label for="amount">Amount</label>
-                        </div>
-                        <div class="d-grid gap-2">
-                            <button class="btn btn-primary" type="submit">TRANSFER</button>
-                        </div>
+            {{Form::open(['route' => 'site.transfer.submit', 'class' => 'form-container mt-5 border p-4 bg-light shadow'])}}
+                <a href="{{ route('site.index') }}"><button type="button" class="btn-close" aria-label="Close"></button></a>
+                <h3 class="my-3 text-secondary">Withdraw</h3>
+                @if(Session::has('message'))
+                    <p class="alert alert-success">{{Session::get('message')}}</p>
+                @endif
+                <div class="row-mx-5">
+                    <div class="form-floating">
+                        {{Form::select('account_number', $bankAccounts, null, ['class' => 'form-select'])}}
+                        <label for="account_number">Account</label>
                     </div>
-                {{Form::close()}}
-            </div>
+                    <div class="form-floating my-3">
+                        {{Form::text('receiver', null, ['class' => 'form-control', 'placeholder' => 'Ex. 2022-321123-0'])}}
+                        <label for="receiver">Transfer To</label>
+                    </div>
+                    <div class="form-floating my-3">
+                        {{Form::number('amount', null, ['class' => 'form-control', 'placeholder' => 200.00])}}
+                        <label for="amount">Amount</label>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-primary" type="submit">TRANSFER</button>
+                    </div>
+                </div>
+            {{Form::close()}}
         </div>
     </div>
 </div>

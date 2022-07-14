4874c7775ef35833913e853ac014c998bb89874d
Paul Kristopher Espina <paulespina@xolveit.com>
2022-07-14 13:37:42 +0800

    add logo admin dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,6 +6,9 @@
 <div class="cards-container">
     <div class="card card-one">
         <header>
+            <div class="avatar">
+                <img src="{{ asset('storage/Logo_Black_Square.jpg') }}" alt="Logo" />
+            </div>
         </header>
         <h3 class="mb-5">{{ Auth::user()->first_name . " " . (Auth::user()->middle_name ? Auth::user()->middle_name . " " : "") . Auth::user()->last_name }}</h3>
         <div class="desc">

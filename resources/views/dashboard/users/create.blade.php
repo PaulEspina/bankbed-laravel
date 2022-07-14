6caf8d1b8c3f05e5e5cced9aebf13e2d9389ded4
Paul Kristopher Espina <paulespina@xolveit.com>
2022-07-14 11:34:37 +0800

    users view update UI

--- resources/views/dashboard/users/create.blade.php
+++ resources/views/dashboard/users/create.blade.php
@@ -1,40 +1,78 @@
 @extends('layouts.app')
  
-@section('title', 'Users')
+@section('title', 'Create User')
  
 @section('content')
-    <h1>Create User</h1>
-    <button>
-        <a href="{{ route('dashboard.users.index') }}">Back</a>
-    </button>
-
-    {{Form::open(['route' => 'dashboard.users.store'])}}
-    <div>
-        {{Form::label('first_name', 'First Name')}}
-        {{Form::text('first_name')}}
+<div class="container-fluid bg">
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="form-fields">
+                {{ Form::open(['route' => 'dashboard.users.store', 'method' => 'POST', 'class' => 'form-container mt-5 border p-4 bg-light shadow']) }}
+                    <h3 class="mb-3 text-secondary">Create User</h3>
+                    @foreach ($errors->all() as $error)
+                        <li class="alert alert-danger">{{ $error }}</li>
+                    @endforeach
+                    <div class="row">
+                        <div class="mb-3 col-md">
+                            <label for="username">Username<span class="text-danger">*</span></label>
+                            {{Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Ex. User123', 'required'])}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col-md">
+                            <label for="password">Password<span class="text-danger">*</span></label>
+                            {{Form::password('password', ['class' => 'form-control', 'placeholder' => 'Ex. secretpassword123', 'required'])}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col-md">
+                            <label for="first_name">First Name<span class="text-danger">*</span></label>
+                            {{Form::text('first_name', null, ['class' => 'form-control', 'placeholder' => 'Ex. Neil Ritsard', 'required'])}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col-md">
+                            <label for="middle_name">Middle Name</label>
+                            {{Form::text('middle_name', null, ['class' => 'form-control', 'placeholder' => 'Ex. '])}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col-md">
+                            <label for="last_name">Last Name<span class="text-danger">*</span></label>
+                            {{Form::text('last_name', null, ['class' => 'form-control', 'placeholder' => 'Ex. Punzalan', 'required'])}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col-md">
+                            <label>Role<span class="text-danger">*</span></label>
+                            {{Form::select('role', ['user' => 'User', 'admin' => 'Admin'], null, ['class' => 'form-control'])}}
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-md">
+                            <a href="{{ route('dashboard.users.index') }}" style="text-decoration:none">
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-primary" type="button">BACK</button>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md">
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-success" type="submit">CREATE</button>
+                            </div>
+                        </div>
+                    </div>
+                {{ Form::close() }}
+            </div>
+        </div>
     </div>
-    <div>
-        {{Form::label('middle_name', 'Middle Name')}}
-        {{Form::text('middle_name')}}
-    </div>
-    <div>
-        {{Form::label('last_name', 'Last Name')}}
-        {{Form::text('last_name')}}
-    </div>
-    <div>
-        {{Form::label('username', 'Username')}}
-        {{Form::text('username')}}
-    </div>
-    <div>
-        {{Form::label('password', 'Password')}}
-        {{Form::text('password')}}
-    </div>
-    <div>
-        {{Form::label('role', 'Role')}}
-        {{Form::text('role')}}
-    </div>
-    <button type="submit">Submit</button>
-    {{Form::close()}}
 </div>
+
+@push('head')
+    <link rel="stylesheet" href="{{ asset('css/card.css')}}" />
+@endpush
+
+@push('scripts')
+@endpush
 
 @endsection
--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -3,32 +3,24 @@
 @section('title', 'Users')
  
 @section('content')
-    <h1>Users</h1>
-    
-    <button>
-        <a href="{{ route('dashboard.index') }}">Back</a>
-    </button>
-    <button>
-        <a href="{{ route('dashboard.users.create') }}">Create</a>
-    </button>
-    <div>
-        <table>
-            <thead>
-                <tr>
-                <th>ID</th>
+<div class="header_fixed">
+    <table>
+        <thead>
+            <tr>
+                <th>ID No.</th>
                 <th>Username</th>
-                <th>First Name</th>
-                <th>Middle Name</th>
-                <th>Last Name</th>
+                <th>Firstname</th>
+                <th>Middlename</th>
+                <th>Lastname</th>
                 <th>Role</th>
-                <th>Updated At</th>
+                <th>Update At</th>
                 <th>Created At</th>
-                <th width="1%">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($users as $key => $user)
-                <tr key={{ $key }}>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($users as $key => $user)
+            <tr key ={{ $key }}>
                 <td>{{ $user->id }}</td>
                 <td>{{ $user->username }}</td>
                 <td>{{ $user->first_name }}</td>
@@ -38,24 +30,46 @@
                 <td>{{ date_format($user->updated_at,'m/d/Y') }}</td>
                 <td>{{ date_format($user->created_at,'m/d/Y') }}</td>
                 <td>
-                    <button>
-                        <a href="{{ route('dashboard.users.show', $user->id) }}">Show</a>
-                    </button>
-                    <button>
-                        <a href="{{ route('dashboard.users.edit', $user->id) }}">Edit</a>
-                    </button>
-                    {{Form::open(['url' => route('dashboard.users.destroy', $user), 'method' => 'delete'])}}
-                        <button type="submit">Delete</button>
-                    {{Form::close()}}
+                    <a href="{{ route('dashboard.users.edit', $user->id) }}"><button>Edit</button></a>
+                    <button data-toggle="modal" data-target="#deleteModal{{$user->id}}">Delete</button>
                 </td>
-                </tr>
-                @empty
-                <tr>
+            </tr>
+            <div class="modal fade" id="deleteModal{{$user->id}}" tabindex="-1" role="dialog" aria-labelledby="modalLabel{{$user->id}}" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalLabel{{$user->id}}">Are you sure?</h5>
+                        </div>
+                        <div class="modal-body">
+                            By clicking the <b>"Yes, delete the record"</b> button, the data will be <b>permanently deleted</b>.
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            {{Form::open(['url' => route('dashboard.users.destroy', $user), 'method' => 'delete'])}}
+                            <button type="submit" class="btn btn-danger">Yes, delete the record</button>
+                            {{Form::close()}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @empty
+            <tr>
                 <td colspan="100%">---</td>
-                </tr>
-                @endforelse
-            </tbody>
-        </table>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
+    <div class="primary-button">
+        <a href="{{ route('dashboard.index') }}"><button class="btn-primary" type="button">Back</button></a>
+        <a href="{{ route('dashboard.users.create') }}"><button class="btn-primary" type="button">Create</button></a>
     </div>
+</div>
+
+@push('head')
+    <link rel="stylesheet" href="{{ asset('css/table_style.css')}}" />
+@endpush
+
+@push('scripts')
+@endpush
 
 @endsection
